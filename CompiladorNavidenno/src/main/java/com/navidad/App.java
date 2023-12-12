9d82568f07439e070312b9980a64f295fd641a37
Camilo Sanchez <camsanchez@estudiantec.cr>
2023-12-11 22:48:18 -0600

    it tokenizes
    Lexer reads documents and can separete the lexemes

--- CompiladorNavidenno/src/main/java/com/navidad/App.java
+++ CompiladorNavidenno/src/main/java/com/navidad/App.java
@@ -1,5 +1,7 @@
 package com.navidad;
 import java_cup.runtime.*;
+import java.io.*;
+import java.util.*;
 /**
  * Hello world!
  *
@@ -7,6 +9,26 @@ import java_cup.runtime.*;
 
 public class App {
     public static void main(String[] args) {
+        String Mydocument = ".\\src\\test\\java\\com\\navidad\\Carta_a_Santa.txt";
+        try {
+            Reader reader = new BufferedReader(new FileReader(Mydocument));
+            Lexer lexer = new Lexer(reader);
+            System.out.println("Tokens: ");
+
+            while (true) {
+                Symbol symbol = lexer.next_token();
+                if (symbol.sym == 0) {
+                    break;
+                }
+                System.out.println("Token: " + symbol.sym + " " + symbol.value);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Hello World!");
+
+         
     }
 }
