838b954d04acac53cf55bf8f32bd41afa01b6e1b
Camilo Sanchez <camsanchez@estudiantec.cr>
2023-12-19 00:36:02 -0600

    update .cup

--- CompiladorNavidenno/src/main/java/com/navidad/sym.java
+++ CompiladorNavidenno/src/main/java/com/navidad/sym.java
@@ -8,34 +8,34 @@ package com.navidad;
 /** CUP generated interface containing symbol constants. */
 public interface sym {
   /* terminals */
-  public static final int ENVIA_RETURN = 49;
-  public static final int ABRE_CUENTO = 37;
-  public static final int CIERRA_EMPAQUE = 40;
+  public static final int ENVIA_RETURN = 48;
+  public static final int ABRE_CUENTO = 36;
+  public static final int CIERRA_EMPAQUE = 39;
   public static final int COLACHO_INT = 21;
-  public static final int ABRE_REGALO = 41;
+  public static final int ABRE_REGALO = 40;
   public static final int RAYO_FLOAT_DIVISION = 5;
-  public static final int NARRA_PRINT = 51;
+  public static final int NARRA_PRINT = 50;
   public static final int L_JOULUPUKKI_FLOAT = 28;
-  public static final int ELFO_IF = 43;
-  public static final int CIERRA_REGALO = 42;
+  public static final int ELFO_IF = 42;
+  public static final int CIERRA_REGALO = 41;
   public static final int PERSONA = 9;
-  public static final int CARBON = 56;
+  public static final int CARBON = 55;
   public static final int ELFO_LESS_EQUAL = 17;
   public static final int ELFO_GREATER = 12;
-  public static final int ENTREGA = 54;
+  public static final int ENTREGA = 53;
   public static final int SANTA_STRING = 25;
-  public static final int ESCUCHA_READ = 52;
-  public static final int ESTRELLA_MAIN = 60;
+  public static final int ESCUCHA_READ = 51;
+  public static final int ESTRELLA_MAIN = 59;
   public static final int L_SANTA_STRING = 30;
-  public static final int PAPA_NOEL_FALSE = 36;
-  public static final int LOCAL_MUERDAGO = 58;
-  public static final int DUENDE_ELSE = 45;
-  public static final int REVISA_UNTIL = 48;
-  public static final int FIN_REGALO = 53;
-  public static final int CIERRE_CUENTO = 38;
-  public static final int FIESTA_PRIVATE = 34;
-  public static final int HADA_ELIF = 44;
-  public static final int PAPA_NOEL_TRUE = 35;
+  public static final int PAPA_NOEL_FALSE = 35;
+  public static final int LOCAL_MUERDAGO = 57;
+  public static final int DUENDE_ELSE = 44;
+  public static final int REVISA_UNTIL = 47;
+  public static final int FIN_REGALO = 52;
+  public static final int CIERRE_CUENTO = 37;
+  public static final int FIESTA_PRIVATE = 33;
+  public static final int HADA_ELIF = 43;
+  public static final int PAPA_NOEL_TRUE = 34;
   public static final int RELAMPAGO_INT_DIVISION = 4;
   public static final int EOF = 0;
   public static final int L_COLACHO_INT = 27;
@@ -43,32 +43,31 @@ public interface sym {
   public static final int TRUENO_DECREASE = 3;
   public static final int ELFO_GREATER_EQUAL = 16;
   public static final int error = 1;
-  public static final int FESTIVA_PUBLICL = 33;
+  public static final int FESTIVA_PUBLICL = 32;
   public static final int ELFO_LESS = 13;
-  public static final int ABRE_EMPAQUE = 39;
+  public static final int ABRE_EMPAQUE = 38;
   public static final int PAPA_NOEL_BOOLEAN = 23;
   public static final int SALTARIN_ELEVATE = 6;
-  public static final int ENVUELVE_FOR = 46;
+  public static final int ENVUELVE_FOR = 45;
   public static final int COMETA_PRODUCT = 7;
-  public static final int HACE_DO = 47;
+  public static final int HACE_DO = 46;
   public static final int REY_BALTASAR_NOT = 20;
-  public static final int NAVIDAD_ABSTRACT = 32;
+  public static final int NAVIDAD_ABSTRACT = 31;
   public static final int ELFO_NOT_EQUAL = 15;
-  public static final int FUNCTION_VILLANCICOS = 57;
+  public static final int FUNCTION_VILLANCICOS = 56;
   public static final int QUIEN_INCREMENT = 11;
-  public static final int MEDIAS_ERROR = 59;
+  public static final int MEDIAS_ERROR = 58;
   public static final int REY_MELCHOR_COJUNCTION = 18;
   public static final int VIEJITO_PASCUERO_ARRAY = 26;
   public static final int GRINCH_DECREMENT = 10;
-  public static final int L_VIEJITO_PASCUERO_ARRAY = 31;
-  public static final int SEPARADOR_BASTON = 55;
+  public static final int SEPARADOR_BASTON = 54;
   public static final int REY_GASPAR_DISJUNCTION = 19;
   public static final int CUPIDO_MODULE = 8;
   public static final int RODOLFO_SUM = 2;
   public static final int ELFO_EQUAL = 14;
   public static final int JOULUPUKKI_FLOAT = 22;
   public static final int L_CLAUS_CHAR = 29;
-  public static final int CORTA_BREAK = 50;
+  public static final int CORTA_BREAK = 49;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
@@ -101,7 +100,6 @@ public interface sym {
   "L_JOULUPUKKI_FLOAT",
   "L_CLAUS_CHAR",
   "L_SANTA_STRING",
-  "L_VIEJITO_PASCUERO_ARRAY",
   "NAVIDAD_ABSTRACT",
   "FESTIVA_PUBLICL",
   "FIESTA_PRIVATE",
